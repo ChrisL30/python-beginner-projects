b20d2088d25bc58db622f2304c7ddce2391598c3
ChrisL30 <clee3032@gmail.com>
2025-06-07 13:01:46 -0400

    Create booleanpractice.py

---
+++ booleanpractice.py
@@ -0,0 +1,23 @@
+price_per_apple = 0.5
+price_per_orange = 0.5
+price_per_banana = 0.25
+managers_first_name = "Chris"
+cashiers_first_name = "Kris"
+
+
+# equal to and not equal to
+print(price_per_apple == price_per_orange)
+print(price_per_apple==price_per_banana)
+print(price_per_apple == price_per_banana)
+print(price_per_apple != price_per_banana)
+print(price_per_orange != price_per_apple)
+print(managers_first_name == cashiers_first_name)
+
+# greater than / less than
+print(price_per_apple > price_per_banana)
+print(price_per_apple < price_per_banana)
+
+#combination
+print(price_per_apple >= price_per_orange)
+print(price_per_banana >= price_per_apple)
+
