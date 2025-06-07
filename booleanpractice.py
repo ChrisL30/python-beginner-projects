1f11ddeb580ad83c207164c1d5ec6498fb2acf1a
ChrisL30 <clee3032@gmail.com>
2025-06-07 13:35:39 -0400

    Update booleanpractice.py

--- booleanpractice.py
+++ booleanpractice.py
@@ -21,3 +21,31 @@ print(price_per_apple < price_per_banana)
 print(price_per_apple >= price_per_orange)
 print(price_per_banana >= price_per_apple)
 
+# Combining Condition for Booleans (and, or, not)
+price_per_apple = 0.50
+apple_needs_fridge = False
+apples_per_bag = 10
+price_per_mango = 0.75
+mango_needs_fridge = True
+mangos_per_bag = 5
+
+# 'and' Operator
+worth_stocking_apples = (price_per_apple > 0.35) and (apples_per_bag > 8)
+worth_stocking_mangos = (price_per_mango > 1.00) and (mangos_per_bag > 3)
+print(worth_stocking_apples)
+print(worth_stocking_mangos)
+
+
+
+# 'or' Operator
+worth_stocking_apples = (price_per_apple > 0.35) or (apple_needs_fridge == False)
+worth_stocking_mangos = (price_per_mango > 1.00) or (mangos_per_bag > 3)
+print(worth_stocking_apples)
+print(worth_stocking_mangos)
+
+
+# 'not' Operator
+worth_stocking_apples = not(price_per_apple <= 0.35)
+print(worth_stocking_apples)
+worth_stocking_mangos = not(price_per_mango <= 0.90)
+print(worth_stocking_mangos)
